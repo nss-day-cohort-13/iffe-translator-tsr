f4126683840a109acef839238f27489cfef0a90b
Russell Ries <riesru@gmail.com>
2016-04-26 14:56:30 -0500

    add link html with lang.js and script.js

--- js/lang.js
+++ js/lang.js
@@ -1,8 +1,16 @@
-var language = (function () {
+var language = (function() {
 
 
   return {
-
+    setLanguage: function(userChoice) {
+      if (userChoice === "Spanish")
+        console.log("Spanish");
+      if (userChoice === "German") {
+        console.log("German");
+      } else {
+        console.log("Pig Latin");
+      }
+    }
 
 
   };
